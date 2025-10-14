b374068b0e31c66e371ff7aae317c6b48beaae1e
babiker9991-coder <babiker9991@gmail.com>
2025-10-14 11:02:29 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,28 +1,17 @@
-// ضع بيانات مشروع Firebase الخاصة بك هنا
 const firebaseConfig = {
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAWp1YvxwS_RGi9n3j_PPSnTcjM6Y8l-k0",
-  authDomain: "sport-f707c.firebaseapp.com",
-  projectId: "sport-f707c",
-  storageBucket: "sport-f707c.firebasestorage.app",
-  messagingSenderId: "339029738153",
-  appId: "1:339029738153:web:c9bbedfab4f23509879fb8",
-  measurementId: "G-4F1CBCH559"
+  apiKey: "AIzaSyDxoEJLaGcEy7s1P2nE2_bDniS71ldI31Q",
+  authDomain: "alhadari-net.firebaseapp.com",
+  databaseURL: "https://alhadari-net-default-rtdb.firebaseio.com",
+  projectId: "alhadari-net",
+  storageBucket: "alhadari-net.appspot.com",
+  messagingSenderId: "465757130283",
+  appId: "1:465757130283:web:10128c19bef6171e5e246e"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-};
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
 const storage = firebase.storage();
+
+/* ضع UID الخاص بالمشرف بعد إنشاء الحساب في Firebase Auth */
+const ADMIN_UID = "ضع_UID_هنا";
