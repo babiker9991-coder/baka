2017eaf8e7e5079deccde5269a8474a9f75b5946
babiker9991-coder <babiker9991@gmail.com>
2025-10-14 13:47:49 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,15 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAWp1YvxwS_RGi9n3j_PPSnTcjM6Y8l-k0",
+  apiKey: "AIzaSyDxoEJLaGcEy7s1P2nE2_bDniS71ldI31Q",
   authDomain: "alhadari-net.firebaseapp.com",
   databaseURL: "https://alhadari-net-default-rtdb.firebaseio.com",
   projectId: "alhadari-net",
-  storageBucket: "alhadari-net.appspot.com",
+  storageBucket: "alhadari-net.firebasestorage.app",
   messagingSenderId: "465757130283",
-  appId: "1:465757130283:web:10128c19bef6171e5e246e"
+  appId: "1:465757130283:web:10128c19bef6171e5e246e",
+  measurementId: "G-XLQB1M9FHQ"
 };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.database();
 const storage = firebase.storage();
-
-const ADMIN_UID = "ضع_UID_هنا"; // ضع UID المشرف
